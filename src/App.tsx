ae0bed9e3160b624075c6a01455694608f83cb53
MajoApol <mariajoanaceorn@gmail.com>
2026-06-23 23:25:56 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,18 @@ import "./App.css";
 import Login from "./pages/login";
 
 import Dashboard from "./pages/dashboard";
-import Disciplinas from "./pages/disciplinas";
-import TutorIA from "./pages/tutorIA";
 import Profile from "./pages/perfil";
+import TutorIA from "./pages/tutorIA";
 
-import Register from "./pages/register";
 import CompleteRegister from "./pages/completeRegister";
 import ForgotPassword from "./pages/forgotPassword";
 import NewPassword from "./pages/newPassword";
+import Register from "./pages/register";
+import Projects from "./pages/projects";
 
 function App() {
   return (
     <Routes>
-      {/* <Route path="/" element={<InputField />} /> */}
       <Route path="/" element={<Login />} />
       <Route path="/registro" element={<Register />} />
       <Route path="/concluir-registro" element={<CompleteRegister />} />
@@ -23,10 +22,9 @@ function App() {
       <Route path="/nova-senha" element={<NewPassword />} />
 
       <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/dashboard/disciplinas" element={<Disciplinas />} />
+      <Route path="/dashboard/projetos" element={<Projects />} />
       <Route path="/dashboard/tutor-ia" element={<TutorIA />} />
       <Route path="/dashboard/perfil" element={<Profile />} />
-
     </Routes>
   );
 }
